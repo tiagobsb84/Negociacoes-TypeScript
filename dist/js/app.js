3555ee2971aa625181e4b8f3faa7d3dc1c1cef69
Tiago <tiagobsb84@gmail.com>
2022-08-30 19:44:13 -0300

    Convertendo dados de entrada

--- dist/js/app.js
+++ dist/js/app.js
@@ -1,12 +1,10 @@
 import { NegociacaoController } from "./controllers/negociacao-controller.js";
 import { Negociacao } from "./models/negociacao.js";
-
 const negociacao = new Negociacao(new Date(), 100, 10);
 console.log(negociacao.volume);
-
 const controller = new NegociacaoController();
 const form = document.querySelector('.form');
 form.addEventListener('submit', event => {
     event.preventDefault();
     controller.adicionar();
-})
+});
--- dist/js/models/negociacao.js
+++ dist/js/models/negociacao.js
@@ -1,28 +1,19 @@
 export class Negociacao {
-    private _data: Date;
-    private _quantidade: number;
-    private _valor: number;
-
-    constructor(data: Date, quantidade: number, valor: number) {
+    constructor(data, quantidade, valor) {
         this._data = data;
         this._quantidade = quantidade;
         this._valor = valor;
     }
-
-    get data(){
+    get data() {
         return this._data;
     }
-
-    get quantidade(){
+    get quantidade() {
         return this._quantidade;
     }
-
-    get valor(){
+    get valor() {
         return this._valor;
     }
-
-    get volume(){
+    get volume() {
         return this._valor * this._quantidade;
     }
 }
-
