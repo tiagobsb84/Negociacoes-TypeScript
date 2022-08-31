788a2f1bb3d0fb7ec318f25a831e37659f0b7c3d
Tiago <tiagobsb84@gmail.com>
2022-08-31 00:09:42 -0300

    Finalizando

--- app/models/negociacoes.ts
+++ app/models/negociacoes.ts
@@ -1,13 +1,13 @@
 import { Negociacao } from "./negociacao";
 
 export class Negociacoes {
-    private negociacoes: Array<Negociacao> = [];
+    private negociacoes: Negociacao[] = [];
 
     adicionar(negociacao: Negociacao) {
         this.negociacoes.push(negociacao);
     }
 
-    list(): ReadonlyArray<Negociacao> {
+    list(): readonly Negociacao[] {
         return this.negociacoes;
     }
 }
--- dist/js/models/negociacao.js
+++ dist/js/models/negociacao.js
@@ -1,20 +1,15 @@
 export class Negociacao {
-    constructor(_data, _quantidade, _valor) {
+    constructor(_data, quantidade, valor) {
         this._data = _data;
-        this._quantidade = _quantidade;
-        this._valor = _valor;
+        this.quantidade = quantidade;
+        this.valor = valor;
     }
     ;
     get data() {
-        return this._data;
-    }
-    get quantidade() {
-        return this._quantidade;
-    }
-    get valor() {
-        return this._valor;
+        const data = new Date(this._data.getTime());
+        return data;
     }
     get volume() {
-        return this._valor * this._quantidade;
+        return this.valor * this.quantidade;
     }
 }
